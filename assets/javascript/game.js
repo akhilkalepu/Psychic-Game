583da73ca46960949c9eeea8f1fae290b665bf7d
theinfamousAK <akhilkalepu@protonmail.com>
2018-08-18 12:39:58 -0400

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // VARIABLES
-var compLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
@@ -12,37 +12,38 @@ document.onkeyup = function(event) {
 
     var userGuess = event.key;
 
-    var compChoice = compLetters[Math.floor(Math.random() * compLetters.length)];
+    var compChoice = letters[Math.floor(Math.random() * letters.length)];
 
-    if (userGuess === compChoice) {
-        alert "YOU WIN!";
-        wins ++;
-        guessesLeft = 9;
-        userChoices = [];
+    if (letters.indexOf(userGuess) > -1) {
+
+        if (userGuess === compChoice) {
+            wins ++;
+            guessesLeft = 9;
+            userChoices = [];
+        }
+
+        if (userGuess !== compChoice) {
+            guessesLeft --;
+            userChoices.push(userGuess);
+        }
+
+        if (guessesLeft === 0) {
+            guessesLeft = 9;
+            losses ++;
+            userChoices = [];
+        }
+    
     }
 
-    if (userGuess != compChoice) {
-        guessesLeft --;
-        userChoices.push(userGuess);
-    }
-
-    if (guessesLeft === 0) {
-        alert "YOU LOSE!";
-        guessesLeft = 9;
-        losses ++;
-        userChoices = [];
-    }
-
-
-    // DISPLAY VARIABLES
+    // DISPLAY NEW VARIABLES
     var html =
-    "<h1> The Psychic Game </h1>" +
+    "<h1>The Psychic Game</h1>" +
 	"<p>Guess what letter I'm thinking of!</p>" + 
     "<p>Wins: " + wins + "</p>" +
     "<p>Losses: " + losses + "</p>" +   
     "<p>Guesses left: " + guessesLeft + "</p>" +
     "<p>Your guesses so far: : " + userChoices + "</p>";
 
-    document.querySelector('#game').innerHTML = html;
+    document.querySelector("#game").innerHTML = html;
 
 }
